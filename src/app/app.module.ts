4dc5d1336c9f7ea93fc5727886e64fd9f07e0262
c5apple <c5apple_party@yahoo.co.jp>
2019-11-24 16:40:12 +0900

    ログイン,ログアウト準備

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/header/header.module';
+import { LoginModule } from './login/login.module';
+import { LogoutModule } from './logout/logout.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HeaderModule } from './shared/header/header.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    LoginModule,
+    LogoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
