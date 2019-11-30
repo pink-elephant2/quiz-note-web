12a5d518e30c6c73031e81e90286ec95d44a2479
c5apple <c5apple_party@yahoo.co.jp>
2019-12-01 00:38:23 +0900

    ローディング

---
+++ src/app/shared/service/loading/loading.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+/**
+ * ローディングサービス
+ */
+@Injectable()
+export class LoadingService {
+
+  constructor() { }
+
+  /** ローディング検知 */
+  public isLoading = new BehaviorSubject<boolean>(null);
+
+  /**
+   * ローディング状態を設定する
+   */
+  public setLoading(isLoading: boolean) {
+    this.isLoading.next(isLoading);
+  }
+}
