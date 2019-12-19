4166ee0b4c169a013c38ede01c84e4fec3b0865d
c5apple <c5apple_party@yahoo.co.jp>
2019-12-19 17:26:52 +0900

    ドメイン設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiDomain: 'http://ec2-3-112-18-50.ap-northeast-1.compute.amazonaws.com:8080',
+  apiDomain: 'https://api.quiz-note.com',
   analytics: {
     id: 'UA-134451760-2'
   },
