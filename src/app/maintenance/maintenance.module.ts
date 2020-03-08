4c16094db47a4e54349dea8f5a10a0e68aac8349
c5apple <c5apple_party@yahoo.co.jp>
2020-03-08 20:31:18 +0900

    レイアウト調整

--- src/app/maintenance/maintenance.module.ts
+++ src/app/maintenance/maintenance.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MaintenanceComponent } from './maintenance.component';
 import { MaintenanceRoutingModule } from './maintenance-routing.module';
 import { TwitterModule } from 'shared/component/twitter';
+import { ThemeColorModule } from 'shared/directive/theme-color';
 
 @NgModule({
   declarations: [MaintenanceComponent],
   imports: [
     CommonModule,
     MaintenanceRoutingModule,
-    TwitterModule
+    TwitterModule,
+    ThemeColorModule
   ]
 })
 export class MaintenanceModule { }
